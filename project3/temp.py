d027a9924b49f61143aed6e67a66913aa78d6178
Brian Lee <blee025432@gmail.com>
2018-10-10 14:37:17 -0400

    got format to work

--- project3/temp.py
+++ project3/temp.py
@@ -8,7 +8,13 @@ start=time()
 # define helper function format that converts time
 # in tenths of seconds into formatted string A:BC.D
 def format(t):
-    pass
+    t=int(t)
+    if t < 600:
+        filler = (2-len(str(t/10)))*'0'
+        return '%s:%s.%s' % (0,filler + str(t/10),t%10)
+    else:
+        filler = (2-len(str((t%600)/10)))*'0'
+        return '%s:%s.%s' % (t/600,filler + str((t%600)/10),t%10)
     
 # define event handlers for buttons; "Start", "Stop", "Reset"
 def start():
@@ -19,8 +25,8 @@ def create_timer():
     isRunning=True
     iet=0
     while isRunning and iet < 10:
-        sleep(1) #not supported in CodeSkulptor...
-        print(int(10*(time.time()-time())))
+        #sleep(1) not supported in CodeSkulptor...
+        #print(int(10*(time.time()-time())))
         iet+=1
 
 # define draw handler
@@ -34,6 +40,7 @@ def create_timer():
 
 # start frame
 create_timer()
+
 
 # Please remember to review the grading rubric
 
