1c6f9c3b75fd56edf016a1fa3d8f20adb7ee1e5e
Brian Lee <blee025432@gmail.com>
2018-10-12 14:28:19 -0400

    completed project3

--- project3/test.py
+++ project3/test.py
@@ -1,10 +1,11 @@
-import time
 import simplegui
 # template for "Stopwatch: The Game"
 
 # define global variables
 isRunning = False
 elapsed=0
+successes=0
+total=0
 
 # define helper function format that converts time
 # in tenths of seconds into formatted string A:BC.D
@@ -19,32 +20,35 @@ def format(t):
     
 # define event handlers for buttons; "Start", "Stop", "Reset"
 def start():
-    print('a')
-    global isRunning
-    isRunning=True
-    create_timer()
-
+    timer.start()
+def stop():
+    timer.stop()
+def reset():
+    global elapsed
+    global total
+    global successes
+    elapsed=0
+    successes=0
+    total=0
+    
+def capture():
+    global successes
+    global total
+    total+=1
+    if elapsed%10==0 and elapsed != 0:
+        successes+=1
+    
+    
 # define event handler for timer with 0.1 sec interval
 #so apparently there's a CodeSkulptor timer...
-def create_timer():
-    
-    start = time.time()
-    iet=0
+def time_handler():
     global elapsed
-    
-    while isRunning and iet < 30:  
-        end = time.time()
-        deltat = int(10*(end-start))
-        print(deltat)
-        if deltat>=0 and deltat != elapsed:
-            elapsed=deltat
-            print(elapsed)
-            iet+=1
-
+    elapsed+=1
 
 # define draw handler
 def draw(canvas):
-    canvas.draw_text(str(elapsed), [50,112], 48, "White")
+    canvas.draw_text(format(elapsed), [50,112], 48, "White")
+    canvas.draw_text("Success/Total: %s/%s" % (successes,total),(5,20),20,"White")
     
 # create frame
 frame = simplegui.create_frame("Stopwatch", 300, 200)
@@ -52,10 +56,12 @@ frame.set_draw_handler(draw)
 
 # register event handlers
 frame.add_button('Start Time',start)
-
-frame.start()
+frame.add_button('Stop Time',stop)
+frame.add_button('Reset',reset)
+frame.add_button('Capture!',capture)
+timer = simplegui.create_timer(100, time_handler)
 
 # start frame
-
+frame.start()
 
 # Please remember to review the grading rubric
