e0bfc6a7b56ed7f725844be1289c3cdff07460d5
Brian Lee <blee025432@gmail.com>
2018-10-15 15:10:12 -0400

    for loop

--- flask/app/routes.py
+++ flask/app/routes.py
@@ -5,4 +5,14 @@ from flask import render_template
 @app.route('/index')
 def index():
 	user = {'username':'CAEN'}
-	return render_template('index.html',title='Home',user=user)	
+	posts = [
+		{
+			'author':{'username':'Mark Schlissel'},
+			'body':'Beautiful day in Michigan'	
+		},
+		{
+			'author':{'username':'Alec Gallimore'},
+			'body':'Beautiful day in the College of Engineering'
+		}
+	]
+	return render_template('index.html',title='Home',user=user,posts=posts)	
