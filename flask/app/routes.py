08826d64b7452b8c81696fbd0bc9a19d649a7ad8
Brian Lee <blee025432@gmail.com>
2018-10-17 15:14:10 -0400

    gotta look over html

--- flask/app/routes.py
+++ flask/app/routes.py
@@ -1,5 +1,6 @@
 from app import app
 from flask import render_template
+from app.scraper import parse
 
 @app.route('/')
 @app.route('/index')
@@ -19,5 +20,5 @@ def index():
 
 @app.route('/software')
 def software():
-	print(app.root_path)
-	return render_template('template.html')
+	data = parse()
+	return render_template('mytable.html',rows = data[0],headers=data[1])
